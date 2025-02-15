6d8b0d78afe4c5bc8d410e2e0a4291a2dcf970af
Alstudd <alstonsoares17@gmail.com>
2025-02-16 05:00:04 +0530

    web search starter

--- web/app/dashboard/side-nav.tsx
+++ web/app/dashboard/side-nav.tsx
@@ -2,7 +2,7 @@
 
 import { DockIcon, Docks } from "@/components/ui/dock";
 import { cn } from "@/lib/utils";
-import { BarChart2, ClipboardPen, Files, Search } from "lucide-react";
+import { BarChart2, ClipboardPen, Files, Search, Globe } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -45,6 +45,20 @@ export default function SideNav() {
             className={cn(
               "flex flex-row gap-3 min-h-0 flex-1 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
               {
+                "text-green-600": pathname.endsWith("/websearch"),
+              }
+            )}
+            href="/dashboard/websearch"
+          >
+            <Globe />
+            WebSearch
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={cn(
+              "flex flex-row gap-3 min-h-0 flex-1 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+              {
                 "text-green-600": pathname.endsWith("/notes"),
               }
             )}
@@ -69,7 +83,7 @@ export default function SideNav() {
           </Link>
         </li>
       </ul>
-      <div className="sm:hidden block fixed w-[80%] mx-1 bottom-0">
+      <div className="sm:hidden flex justify-center fixed w-[80%] mx-1 bottom-0">
         <Docks
           className="z-20 rounded-t-xl bg-white dark:bg-black shadow-md shadow-second"
           direction="middle"
@@ -88,6 +102,15 @@ export default function SideNav() {
               <Files
                 className={cn("size-6 text-second", {
                   "text-green-600": pathname.endsWith("/documents"),
+                })}
+              />
+            </Link>
+          </DockIcon>
+          <DockIcon>
+            <Link href={"/dashboard/websearch"}>
+              <Globe
+                className={cn("size-6 text-second", {
+                  "text-green-600": pathname.endsWith("/websearch"),
                 })}
               />
             </Link>
--- web/app/dashboard/websearch/page.tsx
+++ web/app/dashboard/websearch/page.tsx
@@ -1,8 +1,8 @@
-export default function SettingsPage() {
+export default function WebSearch() {
   return (
     <main className="w-full space-y-8">
       <div className="flex justify-between items-center">
-        <h1 className="text-4xl font-bold">Settings</h1>
+        <h1 className="text-4xl font-bold">WebSearch</h1>
       </div>
     </main>
   );
