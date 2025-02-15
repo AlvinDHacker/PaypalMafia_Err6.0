d42abb0b98a44f36e2cde1abde41d8b1cfe6df56
AlvinDHacker <alvindsouza2204@gmail.com>
2025-02-15 17:29:28 +0530

    Toggle

--- web/components/ui/mode-toggle.tsx
+++ web/components/ui/mode-toggle.tsx
@@ -1,40 +1,38 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import React, { useEffect, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "./button";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const [darkMode, setDarkMode] = useState(false);
 
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") setDarkMode(true);
+    else setDarkMode(false);
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+    <div>
+      {darkMode ? (
+        <Button onClick={() => setDarkMode(!darkMode)} variant={"outline"}>
+          <Sun />
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+      ) : (
+        <Button onClick={() => setDarkMode(!darkMode)} variant={"outline"}>
+          <Moon />
+        </Button>
+      )}
+    </div>
+  );
 }
